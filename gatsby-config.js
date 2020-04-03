f200e73e7ca3e58f9ac7328cc466fb127346dff8
Valentin Bellamy <bellamyvalentin@gmail.com>
2020-04-03 18:21:30 +0200

    error fixed

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,9 +4,9 @@ require("dotenv").config({
 
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Studio Mitsu`,
+    description: `Studio Mitsu is a graphic design studio.`,
+    author: `@valenbellamy`,
   },
   plugins: [
     "gatsby-plugin-sass",
